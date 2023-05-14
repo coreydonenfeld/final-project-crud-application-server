b5cb11876d594bec896aa34ea0289b487d876233
Corey Donenfeld <corey@studiosimpati.co>
2023-05-14 08:03:34 -0400

    Tiny formatting tweaks

--- app.js
+++ app.js
@@ -15,19 +15,19 @@ const db = require('./database');
 /* MODEL SYNCHRONIZATION & DATABASE SEEDING */
 // Set up sync and seed process
 const syncDatabase = async () => {
-  try {
-    // Model Synchronization:
-    // - Make a connection between the Node.js application (this server app) and the Postgres database application.
-    // - Create new tables (according to the models) in the Postgres database application, dropping tables first if they already existed
-    await db.sync({force: true});  // Drop table if already exists (force: true)
-    console.log('------Synced to db--------')
-    // Database Seeding
-    await seedDB();  
-    console.log('--------Successfully seeded db--------');
-  } 
-  catch (err) {
-    console.error('syncDB error:', err);
-  }  
+    try {
+        // Model Synchronization:
+        // - Make a connection between the Node.js application (this server app) and the Postgres database application.
+        // - Create new tables (according to the models) in the Postgres database application, dropping tables first if they already existed
+        await db.sync({ force: true });  // Drop table if already exists (force: true)
+        console.log('------Synced to db--------')
+        // Database Seeding
+        await seedDB();
+        console.log('--------Successfully seeded db--------');
+    }
+    catch (err) {
+        console.error('syncDB error:', err);
+    }
 }
 
 /* SET UP EXPRESS APPLICATION */
@@ -43,36 +43,36 @@ const apiRouter = require('./routes/index');
 /* CONFIGURE EXPRESS APPLICATION */
 // Create a function to configure the Express application
 const configureApp = async () => {
-  // Middleware to handle request data and response
-  app.use(express.json());  // Set up Express to parse JSON requests and generate JSON responses
-  app.use(express.urlencoded({ extended: false }));  // Express to parse requests encoded in URL format and querystring
+    // Middleware to handle request data and response
+    app.use(express.json());  // Set up Express to parse JSON requests and generate JSON responses
+    app.use(express.urlencoded({ extended: false }));  // Express to parse requests encoded in URL format and querystring
 
-  // Set up the Express application's main top-level route and attach all sub-routes to it
-  // Add main top-level URL path "/api" before sub-routes
-  app.use("/api", apiRouter);  // URL paths: "/students/", "/students/:id", "/campuses/", and "/campuses/:id"
+    // Set up the Express application's main top-level route and attach all sub-routes to it
+    // Add main top-level URL path "/api" before sub-routes
+    app.use("/api", apiRouter);  // URL paths: "/students/", "/students/:id", "/campuses/", and "/campuses/:id"
 
-  // Handle routing error: Page Not Found
-  // It is triggered when a request is made to an undefined route 
-  app.use((req, res, next) => {
-    const error = new Error("Not Found, Please Check URL!");
-    error.status = 404;  // Status code 404 Not Found - resource not found
-    next(error);  // Call Error-Handling Middleware to handle the error
-  });
-  // Routing Error-Handling Middleware:
-  // All Express routes' errors get passed to this when "next(error)" is called
-  app.use((err, req, res, next) => {
-    console.error(err);
-    console.log(req.originalUrl);
-    res.status(err.status || 500).send(err.message || "Internal server error.");  // Status code 500 Internal Server Error - server error
-  });
+    // Handle routing error: Page Not Found
+    // It is triggered when a request is made to an undefined route 
+    app.use((req, res, next) => {
+        const error = new Error("Not Found, Please Check URL!");
+        error.status = 404;  // Status code 404 Not Found - resource not found
+        next(error);  // Call Error-Handling Middleware to handle the error
+    });
+    // Routing Error-Handling Middleware:
+    // All Express routes' errors get passed to this when "next(error)" is called
+    app.use((err, req, res, next) => {
+        console.error(err);
+        console.log(req.originalUrl);
+        res.status(err.status || 500).send(err.message || "Internal server error.");  // Status code 500 Internal Server Error - server error
+    });
 };
 
 /* SET UP BOOT FOR SERVER APPLICATION */
 // Construct the boot process by incorporating all needed processes
 const bootApp = async () => {
-  await createDB();  // Create database (if not exists)
-  await syncDatabase();  // Seed the database
-  await configureApp();  // Start and configure Express application
+    await createDB();  // Create database (if not exists)
+    await syncDatabase();  // Seed the database
+    await configureApp();  // Start and configure Express application
 };
 
 /* START THE SERVER BOOT */
--- routes/campuses.js
+++ routes/campuses.js
@@ -25,44 +25,44 @@ const ash = require('express-async-handler');
 // });
 
 /* GET ALL CAMPUSES */
-router.get('/', ash(async(req, res) => {
-  let campuses = await Campus.findAll({include: [Student]});  // Get all campuses and their associated students
-  res.status(200).json(campuses);  // Status code 200 OK - request succeeded
+router.get('/', ash(async (req, res) => {
+    let campuses = await Campus.findAll({ include: [Student] });  // Get all campuses and their associated students
+    res.status(200).json(campuses);  // Status code 200 OK - request succeeded
 }));
 
 /* GET CAMPUS BY ID */
-router.get('/:id', ash(async(req, res) => {
-  // Find campus by Primary Key
-  let campus = await Campus.findByPk(req.params.id, {include: [Student]});  // Get the campus and its associated students
-  res.status(200).json(campus);  // Status code 200 OK - request succeeded
+router.get('/:id', ash(async (req, res) => {
+    // Find campus by Primary Key
+    let campus = await Campus.findByPk(req.params.id, { include: [Student] });  // Get the campus and its associated students
+    res.status(200).json(campus);  // Status code 200 OK - request succeeded
 }));
 
 /* DELETE CAMPUS */
-router.delete('/:id', ash(async(req, res) => {
-  await Campus.destroy({
-    where: {
-      id: req.params.id
-    }
-  });
-  res.status(200).json("Deleted a campus!");
+router.delete('/:id', ash(async (req, res) => {
+    await Campus.destroy({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.status(200).json("Deleted a campus!");
 }));
 
 /* ADD NEW CAMPUS */
-router.post('/', ash(async(req, res) => {
-  let newCampus = await Campus.create(req.body);
-  res.status(200).json(newCampus);  // Status code 200 OK - request succeeded
+router.post('/', ash(async (req, res) => {
+    let newCampus = await Campus.create(req.body);
+    res.status(200).json(newCampus);  // Status code 200 OK - request succeeded
 }));
 
 /* EDIT CAMPUS */
-router.put('/:id', ash(async(req, res) => {
-  await Campus.update(req.body, {
-    where: {
-      id: req.params.id
-    }
-  });
-  // Find campus by Primary Key
-  let campus = await Campus.findByPk(req.params.id, {include: [Student]});  // Get the campus and its associated students
-  res.status(201).json(campus);  // Status code 201 Created - successful creation of a resource
+router.put('/:id', ash(async (req, res) => {
+    await Campus.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    });
+    // Find campus by Primary Key
+    let campus = await Campus.findByPk(req.params.id, { include: [Student] });  // Get the campus and its associated students
+    res.status(201).json(campus);  // Status code 201 Created - successful creation of a resource
 }))
 
 // Export router, so that it can be imported to construct the apiRouter (app.js)
