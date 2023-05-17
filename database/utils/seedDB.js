0c5c69000d0866e0839395701edfb39713b26a9f
Corey Donenfeld <corey@studiosimpati.co>
2023-05-17 11:05:29 -0400

    Updating seedDB

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -11,19 +11,22 @@ const seedDB = async () => {
     const dummy_campus = await Campus.create({
         name: "Hunter College",
         address: "695 Park Ave, New York, NY 10065",
-        description: "This is a school in New York, New York."
+        description: "This is a school in New York, New York.",
+        imageUrl: "https://s29068.pcdn.co/wp-content/uploads/hunter-campus.jpg",
     });
     // Create a new campus
     const dummy_campus2 = await Campus.create({
         name: "Queens College",
         address: "65-30 Kissena Blvd, Queens, NY 11367",
-        description: "This is a school in Queens, New York."
+        description: "This is a school in Queens, New York.",
+        imageUrl: "https://coursedog-images-public.s3.us-east-2.amazonaws.com/undefined/QNS01%20Clock-tower-image%20for%20catalog%20landing%20page.jpg"
     });
     // Create a new campus
     const dummy_campus3 = await Campus.create({
         name: "Brooklyn College",
         address: "2900 Bedford Ave, Brooklyn, NY 11210",
-        description: "This is a school in Brooklyn, New York."
+        description: "This is a school in Brooklyn, New York.",
+        imageUrl: "https://www.brooklyn.cuny.edu/web/com_socialImages/BrooklynCollegeLibrary_1200x628.jpg",
     });
 
     // Create a new student for a campus
